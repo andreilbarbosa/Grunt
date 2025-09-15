b3387e47f8e32ef253c05af4d332d9375f560aa1
andrei <andreilimabarbosa@outlook.com>
2025-09-15 10:51:36 -0300

    feat(config Replace): Adiciona as configs do replace no Grunt e troca o HREF do index para o endereço criado no replace.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,10 +21,29 @@ module.exports = function(grunt){
                 files: ['src/styles/**/*.less'],
                 tasks:['less:development']
             }
+        },
+        replace:{
+            dev:{
+                options:{
+                    patterns: [{
+                        match: 'ENDERECO_CSS',
+                        replacement: './styles/main.css'
+                    }]
+                },
+                files:[
+                    {
+                        expand: true,
+                        flatten: true,
+                        src:['src/index.html'],
+                        dest:'dest/'
+                    }
+                ]
+            }
         }
     })
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
 
     grunt.registerTask('build', ['less:production']);
     grunt.registerTask('default', ['watch']);
